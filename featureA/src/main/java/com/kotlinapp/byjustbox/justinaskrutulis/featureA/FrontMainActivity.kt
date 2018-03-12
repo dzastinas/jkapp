6b4cca44e2d0a211d6b37064080d00a542d6c84b
justinas.krutulis <justinaskrutulis@gmail.com>
2018-03-12 20:05:13 +0200

    Initial commit

---
+++ featureA/src/main/java/com/kotlinapp/byjustbox/justinaskrutulis/featureA/FrontMainActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlinapp.byjustbox.justinaskrutulis.featureA
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FrontMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_front_main)
+    }
+}
